45a0bffc965f83a33a37308636d1ef401ebb3ebb
weeaudi <gurskaideng00@isd728.org>
2025-03-07 16:17:25 -0600

    Revert "Update apt_installer.sh"
    This reverts commit b0a8006cbcc30202c52aa618a9c7ecb687b80b63.

--- apt_installer.sh
+++ apt_installer.sh
@@ -1,36 +1,4 @@
-#!/bin/bash
-
-# Update package lists
 apt update
-
-# Check and install necessary packages
-packages=(
-    cmake texinfo nasm qemu-system-x86 python3-parted doxygen libparted-dev
-)
-
-for pkg in "${packages[@]}"; do
-    if ! dpkg -s "$pkg" &> /dev/null; then
-        echo "Installing $pkg..."
-        apt install -y "$pkg"
-    else
-        echo "$pkg is already installed. Skipping."
-    fi
-done
-
-# Ensure pip3 is installed
-if ! command -v pip3 &> /dev/null; then
-    echo "pip3 is not installed. Installing..."
-    apt install -y python3-pip
-fi
-
-# Check and install necessary Python packages
-python_packages=(sh pyelftools PyFatFS)
-
-for pypkg in "${python_packages[@]}"; do
-    if ! python3 -c "import $pypkg" &> /dev/null; then
-        echo "Installing Python package: $pypkg"
-        pip3 install "$pypkg"
-    else
-        echo "Python package $pypkg is already installed. Skipping."
-    fi
-done
+apt install cmake texinfo nasm qemu-system-x86-64 python3-parted doxygen libparted-dev -y
+pip3 install sh pyelftools PyFatFS
+chmod +r /boot/vmlinuz-*
