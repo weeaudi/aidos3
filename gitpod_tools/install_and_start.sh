0cd667cc6e31a176fdc08941d810bce61f1b0352
weeaudi <48772079+weeaudi@users.noreply.github.com>
2025-03-07 18:53:24 -0600

    Delete gitpod_tools directory

--- gitpod_tools/install_and_start.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt install tightvncserver xfce4 xfce4-goodies -y
-vncserver
-vncserver -kill :1
-echo "startxfce4" >> ~/.vnc/xstartup
-vncserver
-./novnc/utils/novnc_proxy --vnc 0.0.0.0:5901
